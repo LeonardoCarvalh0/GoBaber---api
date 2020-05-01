66c3a774d5c583215d1e8cf655cd780f2992783e
Leonardo Carvalho <leo.carvalhodev@gmail.com>
2020-05-01 16:25:07 -0300

    Auth Middleware

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -13,6 +13,24 @@ class UserController {
     const { id, name, email, provider } = await User.create(req.body);
     return res.json({ id, name, email, provider });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+    if (email && email !== user.email) {
+      const verifyEmail = await User.findOne({ where: { email } });
+      if (verifyEmail) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
 }
 
 export default new UserController();
