fa5f82ce1f166dc6ed4707bd9b127e15d9772ed4
Leonardo Carvalho <leo.carvalhodev@gmail.com>
2020-05-12 16:36:49 -0300

    Configurando Multer

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import User from '../app/models/User';
+import File from '../app/models/File';
 import configDatabase from '../config/database';
 
-const models = [User];
+const models = [User, File];
 class Database {
   constructor() {
     this.init();
@@ -10,7 +11,12 @@ class Database {
 
   init() {
     this.connection = new Sequelize(configDatabase);
-    models.map((model) => model.init(this.connection));
+
+    models
+      .map((model) => model.init(this.connection))
+      .map(
+        (model) => model.associate && model.associate(this.connection.models)
+      );
   }
 }
 export default new Database();
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import MiddlewareAuth from './app/middlewares/auth';
+import multerConfig from './config/multer';
+import fileController from './app/controllers/FileController';
 
 const routes = new Router();
-
+const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(MiddlewareAuth);
 routes.put('/users', UserController.update);
 
+routes.post('/files', upload.single('file'), fileController.store);
 export default routes;
